e8480ac1a33a882f6f7dec25c6810d9e6de733c0
Mayank Dhania <mayank@vikingsasquatch.com>
2021-05-06 15:56:49 +0530

    added netlify form handler

--- js/custom.js
+++ js/custom.js
@@ -161,3 +161,17 @@ function count($this) {
         }, 15);
     }
 }
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  let myForm = document.getElementById('contact-form');
+  let formData = new FormData(myForm)
+  fetch('/', {
+    method: 'POST',
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString()
+  }).then(() => console.log('Form successfully submitted')).catch((error) =>
+    alert(error))
+}
